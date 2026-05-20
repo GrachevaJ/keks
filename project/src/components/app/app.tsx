b5cc0fb0015cc64420d8740355d247984b24d05f
GrachevaJ <gra.che.va@yandex.ru>
2026-05-20 18:09:15 +0700

    Добавляет маршрутизацию

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,9 +1,39 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Main from '../../pages/main/main';
+import Login from '../../pages/login/login';
+import Favourites from '../../pages/favourites/favourites';
+import Catalog from '../../pages/catalog/catalog';
+import ProductPage from '../../pages/product-page/product-page';
+import ErrorPage from '../../pages/error-page/error-page';
+import SignUp from '../../pages/sign-up/sign-up';
+import PrivateRoute from '../private-route/private-route';
+import { AppRoute, AuthorizationStatus } from '../../const';
+
 
 type AppProps = {
   cardsCount: number;
 }
 
-const App = ({cardsCount}: AppProps): JSX.Element => <Main cardsCount={cardsCount}/>;
+const App = ({cardsCount}: AppProps): JSX.Element => (
+  <BrowserRouter>
+    <Routes>
+      <Route index element={<Main cardsCount={cardsCount} />} />
+      <Route path={AppRoute.Login} element={<Login />} />
+      <Route path={AppRoute.SignUp} element={<SignUp />} />
+      <Route path={AppRoute.Favourites}
+        element={
+          <PrivateRoute
+            authorizationStatus={AuthorizationStatus.NoAuth}
+          >
+            <Favourites />
+          </PrivateRoute>
+        }
+      />
+      <Route path={`${AppRoute.ProductPage}/:id`} element={<ProductPage />} />
+      <Route path={AppRoute.Catalog} element={<Catalog />} />
+      <Route path="*" element={<ErrorPage />} />
+    </Routes>
+  </BrowserRouter>
+);
 
 export default App;
---
+++ project/src/const.ts
@@ -0,0 +1,13 @@
+export enum AuthorizationStatus {
+  Auth = 'AUTH',
+  NoAuth = 'NO_AUTH',
+  Unknown = 'UNKNOWN'
+}
+export enum AppRoute {
+  Root = '/',
+  Login = '/login',
+  SignUp = '/sign-up',
+  Favourites = '/favourites',
+  Catalog = '/catalog',
+  ProductPage = '/product-page'
+}
